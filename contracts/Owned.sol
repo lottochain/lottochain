0604832202f9914dfca951ed990c074c576e58c1
Paulo Felipe <paulofelipe@gmail.com>
2017-08-22 23:57:08 +1000

    First draft of contracts

---
+++ contracts/Owned.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.0;
+
+contract owned{
+    address owner;
+    
+    modifier onlyowner(){
+        if (msg.sender == owner){
+            _;
+        }
+        
+    }
+    
+    function owned(){
+        owner = msg.sender;
+        
+    }
+    
+}
+
+contract mortal is owned{
+    function kill(){
+        if(msg.sender == owner)
+            selfdestruct(owner);
+    }
+}
